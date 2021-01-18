1bcc342ccd60f2d7e5a501b44d36a5d516c8eed1
Nilton Pontes <nilton.pontes@jbq.global>
2021-01-18 18:45:38 -0300

    feat: game complete

---
+++ Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    private bool isGameRunning;
+    private int score;
+    private int currentLevelIndex;
+
+    public ObstacleGenerator generator;
+    public GameConfiguration config;
+    public TextMeshProUGUI scoreLabel;
+    public GameUI gameStartUI;
+    public GameUI gameOverUI;
+    public Player player;
+    public LevelConfiguration[] levels;
+
+    void Start()
+    {
+        currentLevelIndex = 0;
+        isGameRunning = false;
+        generator.StopGenerator();
+        gameStartUI.Show();
+    }
+
+    private void Update()
+    {
+        scoreLabel.text = score.ToString("000000000.##");
+
+        if(!isGameRunning) return;
+        score++;
+        CheckLevelUpdate();
+    }
+
+    private void CheckLevelUpdate()
+    {
+        if(currentLevelIndex >= levels.Length - 1) return;
+        if(score < levels[currentLevelIndex + 1].minScore) return;
+        currentLevelIndex++;
+        print(currentLevelIndex);
+        SetCurrentLevelConfiguration();
+    }
+
+    private void SetCurrentLevelConfiguration()
+    {
+        LevelConfiguration level = levels[currentLevelIndex];
+        config.speed = level.speed;
+        config.minRangeObstacleGenerator = level.minRangeObstacleGenerator;
+        config.maxRangeObstacleGenerator = level.maxRangeObstacleGenerator;
+    }
+
+    public void GameStart()
+    {
+        isGameRunning = true;   
+        currentLevelIndex = 0;
+        SetCurrentLevelConfiguration();
+
+        score = 0;
+        generator.GenerateObstacles();
+        gameStartUI.Hide();
+        player.SetActive();
+    }
+    
+    public void GameOver()
+    {
+        isGameRunning = false;
+
+        config.speed = 0f;
+        generator.StopGenerator();
+        gameOverUI.Show();
+    }
+
+    public void RestartGame()
+    {
+        gameOverUI.Hide();
+        generator.ResetGenerator();
+        GameStart();
+    }
+}
---
+++ Assets/Game/Scripts/GameUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    private Animator animator;
+    private CanvasGroup cv;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        cv = GetComponent<CanvasGroup>();
+
+        cv.interactable = false;
+        cv.blocksRaycasts = false;
+    }
+
+    public void Show()
+    {
+        GetComponent<Animator>().Play("FadeInUI");
+        cv.interactable = true;
+        cv.blocksRaycasts = true;
+    }
+
+    public void Hide()
+    {
+        GetComponent<Animator>().Play("FadeOutUI");
+        cv.interactable = false;
+        cv.blocksRaycasts = false;
+    }
+}
